eba9f143e8df78908638e2a83d68b587e570decd
Angelo Rocha <angelo,ofp@gmail.com>
2024-10-10 11:30:49 -0300

    Add some improves on admin front

---
+++ admin/classes/WPSSContentAccess.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WpssUserManager\Admin;
+
+/** Prevent direct access */
+if ( !defined( 'ABSPATH' ) ) {
+	header( 'HTTP/1.0 403 Forbidden' );
+	exit;
+}
+
+/**
+ * Class WPSSContentAccess
+ * @since 1.1.0
+ */
+class WPSSContentAccess {
+
+}
